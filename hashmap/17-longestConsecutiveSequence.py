ac7d17d4b117e9fc0aeb5fc0f440e16a8a4527df
Aditi <aditisaini99@gmail.com>
2025-09-02 15:00:33 -0400

    Reattempted q17 longest consecutive sequence

--- hashmap/17-longestConsecutiveSequence.py
+++ hashmap/17-longestConsecutiveSequence.py
@@ -1,3 +1,6 @@
+from typing import List
+
+#Attempt 1
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         if len(nums)==1:
@@ -14,5 +17,23 @@ class Solution:
                     cur_num+=1
                 max_count = max(count, max_count)
         return max_count 
+
+#Attempt 2
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums)==0:
+            return 0
+        elif len(nums)==0:
+            return 1
+        observed = set()
+        max_seq_len = 1
+        observed = set(nums)
+        for n in observed:
+            if (n-1) not in observed:
+                count = 1
+                while (n+count) in observed:
+                    count+=1
+                max_seq_len = max(max_seq_len, count)
+        return max_seq_len
     
 # Leetcode Link: https://leetcode.com/problems/longest-consecutive-sequence
