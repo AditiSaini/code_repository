84195a69e8cd15f642c1113b7fd0448aa9700611
Aditi <aditisaini99@gmail.com>
2025-09-01 23:35:03 -0400

    Added q29 merge intervals using sorting

---
+++ array/29-mergeIntervals.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals_sorted = sorted(intervals, key=lambda x: x[0])
+        all_intervals = [intervals_sorted[0]]
+        prev_interval = intervals_sorted[0]
+        for idx in range(1, len(intervals_sorted)):
+            prev_interval = all_intervals[-1]
+            interval = intervals_sorted[idx]
+            if prev_interval[1]>=interval[0]:
+                all_intervals.pop()
+                all_intervals.append([prev_interval[0], max(interval[1], prev_interval[1])])
+            else:
+                all_intervals.append(interval)
+        return all_intervals
+
+# Leetcode Link: https://leetcode.com/problems/merge-intervals/
