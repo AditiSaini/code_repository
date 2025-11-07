894af40f079b87f5f5e542231df61f02655abd3b
AditiSaini <aditisaini99@gmail.com>
2025-11-07 02:15:16 -0500

    Updated q66 with optimized version

--- trie/66-trie.py
+++ trie/66-trie.py
@@ -6,41 +6,35 @@ class Node:
 
 class Trie:
     def __init__(self):
-        self.root = Node('root', [], False)
+        self.root = Node('root', {}, False)
 
     def insert(self, word: str) -> None:
         root = self.root
         cur = self.root
         for i in range(len(word)):
             w = word[i]
-            node_exists = False
-            for neigh in cur.next:
-                if neigh.val == w:
-                    node_exists = True
-                    if i==len(word)-1:
-                        neigh.isLeaf = True
-                    cur = neigh
-            if node_exists is False:
-                next_node = Node(w, [], i==len(word)-1)
-                cur.next.append(next_node)
+            if w in cur.next:
+                if i==len(word)-1:
+                    cur.next[w].isLeaf = True
+                cur = cur.next[w]
+            else:
+                next_node = Node(w, {}, i==len(word)-1)
+                cur.next[w] = next_node
                 cur = next_node
 
     def search(self, word: str) -> bool:
         cur = self.root
         for i in range(len(word)):
             w = word[i]
-            node_exists = False
-            for neigh in cur.next:
-                if neigh.val == w:
-                    if i==len(word)-1:
-                        if neigh.isLeaf:
-                            node_exists = True
-                            cur = neigh
+            if w in cur.next:
+                if i==len(word)-1:
+                    if cur.next[w].isLeaf:
+                        cur = cur.next[w]
                     else:
-                        node_exists = True
-                        cur = neigh
-                    break
-            if node_exists is False:
+                        return False
+                else:
+                    cur = cur.next[w]
+            else:
                 return False
         return True
 
@@ -48,26 +42,8 @@ class Trie:
         cur = self.root
         for i in range(len(prefix)):
             w = prefix[i]
-            node_exists = False
-            for neigh in cur.next:
-                if neigh.val == w:
-                    node_exists = True
-                    cur = neigh
-                    break
-            if node_exists is False:
+            if w in cur.next:
+                cur = cur.next[w]
+            else:
                 return False
         return True
-
-    def print_trie(self):
-        def dfs(node, prefix=''):
-            if node.val != 'root':
-                print(prefix + node.val + ('*' if node.isLeaf else ''))
-            for child in node.next:
-                dfs(child, prefix + '  ')
-        dfs(self.root)
-
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
