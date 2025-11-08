50655501c9582cf1110b01dca117bf075c0e27f5
Aditi <aditisaini99@gmail.com>
2025-11-08 16:09:31 -0500

    Updated q33 validate bst with recursion

--- bst/33-validateBST.py
+++ bst/33-validateBST.py
@@ -7,7 +7,7 @@ class TreeNode:
 class Solution:
     def __init__(self):
         self.prev = float('-inf')
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+    def isValidBST1(self, root: Optional[TreeNode]) -> bool:
         if root is None:
             return True
         if not self.isValidBST(root.left):
@@ -16,5 +16,18 @@ class Solution:
             return False
         self.prev = root.val
         return self.isValidBST(root.right)
+    def isValidBST2(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return 
+        left = right = True
+        if root.left:
+            left = self.isValidBST(root.left)
+        if self.prev is not None:
+            if root.val<=self.prev:
+                return False
+        self.prev = root.val
+        if root.right:
+            right = self.isValidBST(root.right)
+        return left and right
 
 # Leetcode Link: https://leetcode.com/problems/validate-binary-search-tree/
