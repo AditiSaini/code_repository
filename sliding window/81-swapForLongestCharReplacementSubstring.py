009a37f0b147e05d7939e8cebf3087afb8f94ff5
AditiSaini <aditisaini99@gmail.com>
2025-11-23 15:36:32 -0500

    Updated q81 swap for longest char replacement

--- sliding window/81-swapForLongestCharReplacementSubstring.py
+++ sliding window/81-swapForLongestCharReplacementSubstring.py
@@ -1,31 +1,31 @@
 class Solution:
-    def findMaxInDict(self, freq):
-        max_freq = 0
-        max_char = ""
+    def getMaxFreqDict(self, freq):
+        maxChar = ""
+        maxFreq = 0
         for ch in freq:
-            if freq[ch]>max_freq:
-                max_char = ch
-                max_freq = freq[ch]
-        return max_freq, max_char
+            if freq[ch]>maxFreq:
+                maxFreq = freq[ch]
+                maxChar = ch
+        return maxFreq, maxChar
+
     def maxRepOpt1(self, text: str) -> int:
-        left = 0
-        window_freq = defaultdict(int)
-        window_size = 0
         max_freq = 0
-        longest_size = 0
         all_freq = defaultdict(int)
         for ch in text:
             all_freq[ch]+=1
+        left = 0 
+        window = 0
+        freq_window = defaultdict(int)
+        longest = 0
         for right, ch in enumerate(text):
-            window_freq[ch]+=1
-            max_freq, max_char = self.findMaxInDict(window_freq)
-            window_size = right-left+1
-            while (window_size - max_freq > 1 or window_size > all_freq[max_char]) and left + 1 < len(text):
-                window_freq[text[left]]-=1
+            freq_window[ch]+=1
+            max_freq, max_char = self.getMaxFreqDict(freq_window)
+            window = right-left+1
+            while window-max_freq>1:
+                freq_window[text[left]]-=1
                 left+=1
-                window_size-=1
-                max_freq, max_char = self.findMaxInDict(window_freq)
-            longest_size = max(longest_size, window_size)
-        return longest_size
+                window-=1
+            longest = max(longest, min(window, all_freq[max_char]))
+        return longest 
             
 # Leetcode Link : https://leetcode.com/problems/swap-for-longest-repeated-character-substring/
